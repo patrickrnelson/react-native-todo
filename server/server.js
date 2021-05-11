b8c570a6d48f431df027ac2bc0f73ff1ce75ad14
patrickrnelson <patrickrnelson1@gmail.com>
2021-05-10 19:40:06 -0500

    Working through server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Route includes
+const taskRouter = require('./routes/user.router');
+
+app.use('/api/task', taskRouter);
+
+const PORT = 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
