acf8554ae552bddb23f13ffe12c87c6e59a20c7b
patrickrnelson <patrickrnelson1@gmail.com>
2021-05-10 19:27:25 -0500

    two pages with bottom nav built out

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Switch, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import HomeScreen from './src/Components/Homepage'
+import AddTaskScreen from './src/Components/AddTask'
+
+const Tab = createBottomTabNavigator();
+
+export default function App() { 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Add" component={AddTaskScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +27,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%'
   },
+  title: {
+    fontSize: 26,
+    marginTop: 150,
+    marginBottom: 25,
+  },
+  item: {
+    marginTop: 10,
+  },
+  listView: {
+    width: '80%',
+  },
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
+  }
 });
