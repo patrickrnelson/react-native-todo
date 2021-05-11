b1b66eb04126908d5856f154a9eff898aa416195
patrickrnelson <patrickrnelson1@gmail.com>
2021-05-10 20:01:38 -0500

    GET route is functional

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -2,4 +2,17 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  console.log('In GET all tasks endpoint');
+  pool.query('SELECT * FROM "tasks"')
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log('Error in GET all')
+      res.sendStatus(500)
+    })
+  }
+)
+
 module.exports = router;
